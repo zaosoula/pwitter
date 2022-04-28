7cae049f2b985d8e4bba61a54692abcf6a17aa7a
Zao Soula <contact@zaosoula.fr>
2022-04-28 09:55:33 +0200

    feat(app): add PostForm component

---
+++ app/components/PostForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="new-postbox">
+    <figure>
+      <img :src="profilePicture" alt="">
+    </figure>
+    <div class="newpst-input">
+      <form method="post" @submit.prevent="handleSubmit">
+        <textarea rows="2" placeholder="write something" v-model="formInputs.content"></textarea>
+        <div class="attachments">
+          <ul>
+            <!-- <li>
+              <i class="fa fa-music"></i>
+              <label class="fileContainer">
+                <input type="file">
+              </label>
+            </li>
+            <li>
+              <i class="fa fa-image"></i>
+              <label class="fileContainer">
+                <input type="file">
+              </label>
+            </li>
+            <li>
+              <i class="fa fa-video-camera"></i>
+              <label class="fileContainer">
+                <input type="file">
+              </label>
+            </li>
+            <li>
+              <i class="fa fa-camera"></i>
+              <label class="fileContainer">
+                <input type="file">
+              </label>
+            </li> -->
+            <li>
+              <button type="submit">Post</button>
+            </li>
+          </ul>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useProfilePicture } from '~/hooks/profilePicture';
+  import { useContext } from '@nuxtjs/composition-api';
+
+  const emit = defineEmits(['submit']);
+
+  const { $axios, $swal } = useContext();
+  const profilePicture = useProfilePicture();
+
+  const defaultFormInputs = {
+    content: '',
+  }
+
+  let formInputs = $ref({
+    ...defaultFormInputs
+  })
+
+  const resetForm = () => {
+    formInputs = {
+      ...defaultFormInputs
+    };
+  }
+
+  const handleSubmit = async () => {
+    const response = await $axios.post('/posts', formInputs);
+
+
+    if(response.status !== 201) {
+      return $swal('An unexpected error occured');
+    }
+
+    emit('submit', {values: formInputs, response});
+    resetForm();
+
+  }
+</script>
