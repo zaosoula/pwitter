169e2b827af0508f290cf712277268c9ac691f09
Zao Soula <contact@zaosoula.fr>
2022-04-28 09:48:30 +0200

    feat(app): add layouts

---
+++ app/layouts/auth.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="theme-layout">
+	<div class="container-fluid pdng0">
+		<div class="row merged">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="land-featurearea">
+
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<Nuxt />
+			</div>
+		</div>
+	</div>
+</div>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="theme-layout">
+	<Header />
+  <Nuxt />
+</div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+</script>
