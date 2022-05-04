593c5af60a1b754f448f98f844977bfdc4ed7227
Zao Soula <contact@zaosoula.fr>
2022-05-04 13:11:38 +0200

    refactor(app/Post): replace dayjs by vanilla js

--- app/components/Post.vue
+++ app/components/Post.vue
@@ -1,13 +1,7 @@
 <script lang="ts" setup>
-  import dayjs from 'dayjs';
-  import LocalizedFormat from 'dayjs/plugin/LocalizedFormat';
-
   import { useProfilePicture } from '~/hooks/profilePicture';
   import { useEnrichContent } from '../hooks/enrichContent';
   import { Post } from '~/models/post';
-
-  dayjs.extend(LocalizedFormat)
-
 
   const { data: post } = defineProps<{
     data: Post
@@ -21,30 +15,35 @@
     }
 
     const plural = (num: number) => num !== 1 ? 's' : '';
+    const format = (value: number, unit: string) => `${value} ${unit}${plural(value)} ago`;
 
     let diff = (Date.now() - time.getTime()) / 1000;
-    console.log(Date.now(), time.getTime(), diff);
+
     if (diff<60) {
-      return `${diff} second${plural(diff)} ago`;
+      return format(diff, 'second');
     }
 
     diff = Math.round(diff/60);
     if (diff<60) {
-      return `${diff} minute${plural(diff)} ago`;
+      return format(diff, 'minute');
     }
+
     diff = Math.round(diff/60);
     if (diff<24){
-      return `${diff} hour${plural(diff)} ago`;
+      return format(diff, 'hour');
     }
+
     diff = Math.round(diff/24);
     if (diff<7){
-      return `${diff} day${plural(diff)} ago`;
+      return format(diff, 'day');
     }
+
     diff = Math.round(diff/7);
     if (diff<4){
-      return `${diff} week${plural(diff)} ago`;
+      return format(diff, 'week');
     }
-    return `on ${dayjs(time).format('LL')}`;
+
+    return `on ${time.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}`;
   }
 </script>
 
