d05e2302dcbb62e4a81094ac59420f2a8db66707
Zao Soula <contact@zaosoula.fr>
2022-05-04 22:50:14 +0200

    refactor(api/route): explicitly define each route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
   scope :api, defaults: {format: :json} do    
-    get '/feed', to: 'feed#index'
-    get '/feed/hashtag/:hashtag', to: 'feed#hashtag'
-    get '/feed/user/:id', to: 'feed#user'
+    get '/feed', to: 'feed#index' # Get feed
+    get '/feed/hashtag/:hashtag', to: 'feed#hashtag' # Get hashtag's feed
+    get '/feed/user/:id', to: 'feed#user' # Get user's feed
     
-    resources :follows, only: [:get, :create, :update, :destroy]
+    post '/follows', to 'follows#create' # Follow a user
+    delete '/follows/:id', to 'follows#destroy' # Unfollow a user
 
-
-    resources :posts
-    post '/posts/:id/like', to: 'likes#create'
-    delete '/posts/:id/like', to: 'likes#destroy'
-
+    post '/posts', to: 'posts#create' # Create post
+    delete '/posts', to: 'posts#destroy' # Delete post
+    post '/posts/:id/like', to: 'likes#create' # Like a post
+    delete '/posts/:id/like', to: 'likes#destroy' # Unlike a post
 
     devise_for :users, controllers: {
       sessions: 'sessions',
       registrations: 'registrations'
     }
     devise_scope :user do
-      get 'users/current', to: 'sessions#show'
-      get 'users/:id', to: 'users#show'
-      get 'users/:id/followers', to: 'follows#followers'
-      get 'users/:id/followings', to: 'follows#followings'
-      get 'users/:id/feed', to: 'feed#user'
+      get 'users/current', to: 'sessions#show' # Get current user
+      get 'users/:id', to: 'users#show' # Get user
+      get 'users/:id/followers', to: 'follows#followers' # Get user's followers
+      get 'users/:id/followings', to: 'follows#followings' # Get user's followings
+      get 'users/:id/feed', to: 'feed#user' # Get user's feed
     end
   end
 end
