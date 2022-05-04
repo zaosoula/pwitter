10a0c0f57b57221d7f851ff0345461c93cf6b483
Zao Soula <contact@zaosoula.fr>
2022-05-04 20:10:01 +0200

    refactor(api/routing): reorganize routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  scope :api, defaults: {format: :json} do
-    
-    resources :posts
+  scope :api, defaults: {format: :json} do    
+    get '/feed', to: 'feed#index'
+    get '/feed/hashtag/:hashtag', to: 'feed#hashtag'
     get '/feed/user/:id', to: 'feed#user'
     
     resources :follows, only: [:get, :create, :update, :destroy]
 
-    resources :feed, only: [:index]
-    get '/feed/hashtag/:hashtag', to: 'feed#hashtag'
 
+    resources :posts
     post '/posts/:id/like', to: 'likes#create'
     delete '/posts/:id/like', to: 'likes#destroy'
+
 
     devise_for :users, controllers: {sessions: 'sessions'}
     devise_scope :user do
